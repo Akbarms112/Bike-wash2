bc6c30ae2ed5e25872bd17962d1ccf2ae9e11672
Akbarms112 <111769597+Akbarms112@users.noreply.github.com>
2025-06-14 16:34:06 +0530

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ className = "h-8 w-8" }) => {
   return (
     <img 
-      src="./src/assets/images/bikewash_logo.png"
+      src="../src/assets/images/bikewash_logo.png"
       alt="BikeWash Logo"
       className={className}
     />
